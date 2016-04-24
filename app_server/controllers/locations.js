2f2c5a41b5eb46d72422c8035b8e6c40ee300704
benastwood <ben.astwood@gmail.com>
2016-04-24 17:45:10 +0100

    4th commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ module.exports.homelist = function(req, res){
 };
 
 module.exports.locationInfo = function(req, res){
-    res.render('index', {title: 'Location Info'});
+    res.render('location-info', {title: 'Location Info'});
 };
 
 module.exports.addReview = function(req, res){
